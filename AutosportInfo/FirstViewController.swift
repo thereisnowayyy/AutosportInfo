3588826f687fa189724bb3a59a62c668edb5861c
thereisnowayyy <41861742+thereisnowayyy@users.noreply.github.com>
2020-09-10 14:51:28 +0500

    Initial Commit

---
+++ AutosportInfo/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  AutosportInfo
+//
+//  Created by Egor Salnikov on 10.09.2020.
+//  Copyright © 2020 Egor Salnikov. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AutosportInfo/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  AutosportInfo
+//
+//  Created by Egor Salnikov on 10.09.2020.
+//  Copyright © 2020 Egor Salnikov. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
